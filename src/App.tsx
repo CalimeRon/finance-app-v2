6f211dbb01a112b0e6271d2ab83f8b96e130b398
CalimeRon <ron.chamma@payfit.com>
2022-10-10 17:26:51 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,7 @@ function App() {
   const string = 'string';
   return (
     <div className='tarace'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='tamere' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <p>kuku</p>
     </div>
   );
 }
